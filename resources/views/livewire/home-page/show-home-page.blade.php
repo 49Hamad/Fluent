b7ae949d3e53246bebeeb3fc8e697ff73bc62e19
Yacoub Haidari <yacoubhaidari801@gmail.com>
2026-08-18 00:42:43 +0300

    Uncomment Livewire components in home page view to enable display of banner, about us section, services, our work, achievements, testimonials, and contact us page.

--- resources/views/livewire/home-page/show-home-page.blade.php
+++ resources/views/livewire/home-page/show-home-page.blade.php
@@ -1,15 +1,14 @@
 <div>
-    {{-- <livewire:home-page.show-banner /> --}}
-
+    <livewire:home-page.show-banner /> 
     {{-- Start About US --}}
-    {{-- <livewire:home-page.about.show-about-us-page /> --}}
-    {{-- <livewire:home-page.about.show-why-choose-us-page /> --}}
-    {{-- <livewire:home-page.about.show-number-talk-page /> --}}
-    {{-- End About US --}}
+    <livewire:home-page.about.show-about-us-page /> 
+    <livewire:home-page.about.show-why-choose-us-page /> 
+    <livewire:home-page.about.show-number-talk-page /> 
+    {{-- End About US  --}}
 
-    {{-- <livewire:home-page.show-service-page /> --}}
-    {{-- <livewire:home-page.show-our-work-page /> --}}
-    {{-- <livewire:home-page.show-achievement-page /> --}}
-    {{-- <livewire:home-page.show-testimonial-page /> --}}
-    {{-- <livewire:home-page.show-contact-usl-page /> --}}
+    <livewire:home-page.show-service-page /> 
+    <livewire:home-page.show-our-work-page /> 
+    <livewire:home-page.show-achievement-page /> 
+    <livewire:home-page.show-testimonial-page /> 
+    <livewire:home-page.show-contact-usl-page />
 </div>
